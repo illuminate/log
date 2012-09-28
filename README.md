23dd562f251d7fbfabb56892eb8c25a157fd354c
Taylor Otwell <taylorotwell@gmail.com>
2012-09-28 17:01:58 -0400

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# Illuminate Log
+
+[![Build Status](https://secure.travis-ci.org/illuminate/log.png)](http://travis-ci.org/illuminate/log)
---
+++ src/Illuminate/Log/Writer.php
@@ -0,0 +1,102 @@
+<?php namespace Illuminate\Log;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger as MonologLogger;
+use Monolog\Handler\RotatingFileHandler;
+
+class Writer {
+
+	/**
+	 * The Monolog logger instance.
+	 *
+	 * @var Monolog\Logger
+	 */
+	protected $monolog;
+
+	/**
+	 * Create a new log writer instance.
+	 *
+	 * @param  Monolog\Logger  $monolog
+	 * @return void
+	 */
+	public function __construct(MonologLogger $monolog)
+	{
+		$this->monolog = $monolog;
+	}
+
+	/**
+	 * Register a file log handler.
+	 *
+	 * @param  string  $path
+	 * @param  string  $level
+	 * @return void
+	 */
+	public function useFiles($path, $level = 'debug')
+	{
+		$level = $this->parseLevel($level);
+
+		$this->monolog->pushHandler(new StreamHandler($path, $level));
+	}
+
+	/**
+	 * Register a rotating file log handler.
+	 *
+	 * @param  string  $path
+	 * @param  int     $days
+	 * @param  string  $level
+	 * @return void
+	 */
+	public function useRotatingFiles($path, $days, $level = 'debug')
+	{
+		$level = $this->parseLevel($level);
+
+		$this->monolog->pushHandler(new RotatingFileHandler($path, $days, $level));
+	}
+
+	/**
+	 * Parse the string level into a Monolog constant.
+	 *
+	 * @param  string  $level
+	 * @return int
+	 */
+	protected function parseLevel($level)
+	{
+		switch ($level)
+		{
+			case 'debug':
+				return MonologLogger::DEBUG;
+
+			case 'info':
+				return MonologLogger::INFO;
+
+			case 'notice':
+				return MonologLogger::NOTICE;
+
+			case 'warning':
+				return MonologLogger::WARNING;
+
+			case 'error':
+				return MonologLogger::ERROR;
+
+			case 'critical':
+				return MonologLogger::CRITICAL;
+
+			case 'alert':
+				return MonologLogger::ALERT;
+
+			case 'emergency':
+				return MonologLogger::EMERGENCY;
+		}
+	}
+
+	/**
+	 * Get the underlying Monolog instance.
+	 *
+	 * @return Monolog\Logger
+	 */
+	public function getMonolog()
+	{
+		return $this->monolog;
+	}
+
+}
